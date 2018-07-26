98a8d1dd577ac4680dc2314e228e720a3144439c
jpachalo <juanlu.pacheco@agilecontent.com>
2018-07-26 12:45:50 +0200

    [PRJCOPENEW-73] - Actualizar javascript para actualización automática de los módulos de parrilla

--- src/commands/GetGuideDataCommand.js
+++ src/commands/GetGuideDataCommand.js
@@ -2,12 +2,17 @@ define(['agile-app', 'config'], function(Agile, config) {
     'use strict';
 
 	return Agile.Command.extend({
-    execute: function (data) {
-      $("[data-selector='cmp-streaming-guide']").load(config.guideURL);
-      $("[data-selector='cmp-all-streaming-guide']").load(config.guideDaylistURL);
-      $("[data-selector='cmp-sticky-guide']").load(config.guideStickyURL);
-      $("[data-selector='cmp-sticky-footer-guide']").load(config.guideFooterURL,this._reloadSliders());
 
+    execute: function (data) {
+      var now = new Date();
+      var limit = 5000; // minimum milliseconds between calls
+      if (!this.lastCalled || (now - this.lastCalled) > limit) {
+        this.lastCalled = now;
+        $("[data-selector='cmp-streaming-guide']").load(config.guideURL);
+        $("[data-selector='cmp-all-streaming-guide']").load(config.guideDaylistURL);
+        $("[data-selector='cmp-sticky-guide']").load(config.guideStickyURL);
+        $("[data-selector='cmp-sticky-footer-guide']").load(config.guideFooterURL, this._reloadSliders());
+      }
     },
     _reloadSliders: function () {
       // Create the event
